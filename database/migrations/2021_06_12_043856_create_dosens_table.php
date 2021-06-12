73d0ed1dfdd611e2d7abcd81c314e5d9a225fb97
Siti Mawaddah <siti.wadd@mhs.unsyiah.ac.id>
2021-06-12 12:06:06 +0700

    make model Dosen --all

---
+++ database/migrations/2021_06_12_043856_create_dosens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDosensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dosens', function (Blueprint $table) {
+            $table->id('nip');
+            $table->string('password');
+            $table->string('nama');
+            $table->string('no_hp', 15);
+            $table->string('email');
+            $table->string('path_foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dosens');
+    }
+}
