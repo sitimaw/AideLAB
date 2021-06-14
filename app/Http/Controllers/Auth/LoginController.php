d290ea5e0d1e3a57330b2b855edc0554dd0dd76c
Siti Mawaddah <siti.wadd@mhs.unsyiah.ac.id>
2021-06-14 15:52:05 +0700

    Autentikasi Aslab

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,8 +7,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
