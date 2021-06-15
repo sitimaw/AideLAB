88baff5e69a6f07a7e1b3d4db9db4a08fe489b1d
Sulthan Zaidan Fauzi <zaidan@mhs.unsyiah.ac.id>
2021-06-15 23:11:15 +0700

    menambahkan halaman aslab terpilih

--- resources/views/components/menu-dosen.blade.php
+++ resources/views/components/menu-dosen.blade.php
@@ -1,12 +1,12 @@
 <ul class="navbar-nav mr-auto menu-aidelab">
     <li class="nav-item active px-2 py-2">
-        <a class="nav-link" href="#">List Aslab<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="{{ route('matakuliah.aslab', session('slug_matakuliah')) }}">List Aslab<span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item active px-2 py-2">
-        <a class="nav-link" href="#">Aslab Terpilih<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="{{ route('matakuliah.aslabTerpilih', session('slug_matakuliah')) }}">Aslab Terpilih<span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item active px-2 py-2">
-        <a class="nav-link" href="#">Materi<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="{{ route('matakuliah.materi', session('slug_matakuliah')) }}">Materi<span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item active px-2 py-2">
         <a class="nav-link" href="#">Roomchat<span class="sr-only">(current)</span></a>
